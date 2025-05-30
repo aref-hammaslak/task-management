0db99b1bccdfaf4b9820e6295d68a62a8a1d92e0
aref-hammaslak <arefhammaslak@gmail.com>
2025-05-30 07:50:11 +0330

    Add database configuration and module for TypeORM integration

---
+++ src/database/database.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../modules/users/models/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('database.host'),
+        port: configService.get('database.port'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        database: configService.get('database.database'),
+        entities: [User],
+        synchronize: true,
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
