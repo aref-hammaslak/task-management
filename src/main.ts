27ae75f4292392f3348bba82d35c67bbbedd0203
aref-hammaslak <arefhammaslak@gmail.com>
2025-06-16 16:52:30 +0330

    Update package-lock.json and main.ts to enhance application configuration and middleware integration. Add cookie-parser and logging middleware, configure CORS settings, and set a global API prefix.

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,36 @@
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
+import { WinstonLogger } from './logger/winston.logger';
+import { LoggingMiddleware } from './middlewares/logging.middleware';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+  const configService = app.get(ConfigService);
+  const logger = WinstonLogger.getLogger(configService);
 
+  app.useLogger(logger);
+  app.use(new LoggingMiddleware().use.bind(new LoggingMiddleware()));
+  app.use(cookieParser());
+  app.enableCors({
+    origin: configService.get<string>('api.corsOrigin'),
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
     }),
   );
+  app.setGlobalPrefix('api');
 
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  const port = configService.get<number>('api.port') || 3000;
+  await app.listen(port);
+
+  logger.log(`Application is running on port ${port}`, 'Bootstrap');
 }
 bootstrap();
