2e8bdfb4326c4164e9ba9bde6679c88956170eaf
aref-hammaslak <arefhammaslak@gmail.com>
2025-07-24 10:04:11 +0330

    Update JWT expiration settings in auth configuration to use numeric values for access and refresh tokens

--- src/configs/auth.config.ts
+++ src/configs/auth.config.ts
@@ -2,7 +2,8 @@ export default () => ({
   auth: {
     JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET || 'access-secret',
     JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET || 'refresh-secret',
-    JWT_ACCESS_EXPIRES_IN: process.env.JWT_ACCESS_EXPIRES_IN || '15m',
-    JWT_REFRESH_EXPIRES_IN: process.env.JWT_REFRESH_EXPIRES_IN || '7d',
+    JWT_ACCESS_EXPIRES_IN: process.env.JWT_ACCESS_EXPIRES_IN || 15 * 60,
+    JWT_REFRESH_EXPIRES_IN:
+      process.env.JWT_REFRESH_EXPIRES_IN || 7 * 24 * 60 * 60,
   },
 });
