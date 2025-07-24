3905f4d0afa508d2215f0c828c5f8fdda37460e5
aref-hammaslak <arefhammaslak@gmail.com>
2025-07-24 10:02:59 +0330

    Rename JwtRefreshGuard to JwtRefreshTokenGuard and refactor JwtRefreshStrategy to simplify validation logic and remove unnecessary dependencies.

--- src/modules/auth/strategies/jwt-refresh.strategy.ts
+++ src/modules/auth/strategies/jwt-refresh.strategy.ts
@@ -1,22 +1,19 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
 import { Request } from 'express';
-import { UsersService } from 'src/modules/users/users.service';
-import { AuthService } from '../auth.service';
-import { CurrentUserWithRefreshToken } from '../types/current-user.type';
+import { CurrentUser } from '../types/current-user.type';
 
 @Injectable()
 export class JwtRefreshStrategy extends PassportStrategy(
   Strategy,
   'jwt-refresh',
 ) {
-  constructor(
-    private configService: ConfigService,
-    private usersService: UsersService,
-    private authService: AuthService,
-  ) {
+  /**
+   * This strategy is used to validate the refresh token from the request.
+   */
+  constructor(private configService: ConfigService) {
     const refreshTokenSecret = configService.get<string>(
       'auth.JWT_REFRESH_SECRET',
     );
@@ -39,20 +36,7 @@ export class JwtRefreshStrategy extends PassportStrategy(
     });
   }
 
-  async validate(req: Request): Promise<CurrentUserWithRefreshToken> {
-    const refreshToken = req.cookies['refreshToken'] as string;
-
-    const currentUser =
-      await this.usersService.findOneByRefreshToken(refreshToken);
-
-    if (!currentUser) {
-      throw new UnauthorizedException('Invalid refresh token');
-    }
-    return {
-      id: currentUser.id,
-      email: currentUser.email,
-      role: currentUser.role,
-      refreshToken: currentUser.refreshToken as string,
-    } as CurrentUserWithRefreshToken;
+  validate(payload: CurrentUser): CurrentUser {
+    return payload;
   }
 }
