ad2aa00ad7e68bffb1e55b1fefaf8394181e2263
aref-hammaslak <arefhammaslak@gmail.com>
2025-07-25 19:07:57 +0330

    feat(users): add user export functionality and enhance findAll method
    - Implement export endpoint to download user data as CSV.
    - Enhance findAll method to include pagination and filtering.
    - Add error handling for cases with no users found.

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -11,6 +11,9 @@ import {
   BadRequestException,
   UseGuards,
   UnauthorizedException,
+  Query,
+  Res,
+  NotFoundException,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -19,6 +22,11 @@ import { User as UserEntity } from './models/user.entity';
 import type { RequestWithUser } from '../auth/types/current-user.type';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { UserByIdPipe } from '../../pipes/user-by-id.pipe';
+import { UsersFilterDto } from './dto/users-filter.dto';
+import { PaginationFilterDto } from './dto/pagination-filter.dto';
+import * as Papa from 'papaparse';
+import type { Response } from 'express';
+import { USER_EXPOSED_FIELDS } from './constants/user-exposed-fields.constant';
 
 @UseGuards(JwtAuthGuard)
 @Controller('users')
@@ -51,15 +59,24 @@ export class UsersController {
   }
 
   @Get()
-  async findAll(): Promise<{
+  async findAll(
+    @Query() filters: UsersFilterDto,
+    @Query() pagination: PaginationFilterDto,
+  ): Promise<{
     success: boolean;
-    data: UserEntity[];
+    data: { users: UserEntity[]; total: number };
     message: string;
   }> {
-    const users = await this.usersService.findAll();
+    const { users, total } = await this.usersService.findAll(
+      filters,
+      pagination,
+    );
+    if (!users.length) {
+      throw new NotFoundException('No users found with the given filters');
+    }
     return {
       success: true,
-      data: users,
+      data: { users, total },
       message: 'Users retrieved successfully',
     };
   }
@@ -93,6 +110,31 @@ export class UsersController {
       data: updatedUser,
       message: 'User profile updated successfully',
     };
+  }
+
+  @Get('export')
+  async export(
+    @Query() filters: UsersFilterDto,
+    @Query() pagination: PaginationFilterDto,
+    @Res() res: Response,
+    @Query('search') search?: string,
+  ) {
+    const { users } = await this.usersService.findAll(
+      filters,
+      pagination,
+      search,
+    );
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    if (!users.length) {
+      throw new NotFoundException('No users found with the given filters');
+    }
+    const csvString = Papa.unparse(users, {
+      header: true,
+      columns: USER_EXPOSED_FIELDS,
+    });
+    res.setHeader('Content-Type', 'text/csv');
+    res.setHeader('Content-Disposition', 'attachment; filename=users.csv');
+    res.send(csvString);
   }
 
   @Get(':id')
