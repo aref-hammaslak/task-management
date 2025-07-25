c2c4645457ba6e44389bfd56e56692be6c6e1fad
aref-hammaslak <arefhammaslak@gmail.com>
2025-07-25 19:04:35 +0330

    refactor(all-exceptions): improve error handling and logging in exception filter
    - Enhance the AllExceptionsFilter to handle QueryFailedError and ValidationError specifically.
    - Update logging to use NestJS Logger instead of WinstonLogger.
    - Include stack trace in development environment for better debugging.

--- src/filters/all-exceptions.filter.ts
+++ src/filters/all-exceptions.filter.ts
@@ -4,30 +4,48 @@ import {
   ExceptionFilter,
   HttpException,
   HttpStatus,
+  Logger,
 } from '@nestjs/common';
 import { Response } from 'express';
-import { WinstonLogger } from '../logger/winston.logger';
+import { ValidationError } from 'class-validator';
+import { QueryFailedError } from 'typeorm';
 
 @Catch()
 export class AllExceptionsFilter implements ExceptionFilter {
-  constructor(private readonly logger: WinstonLogger) {}
+  private readonly logger = new Logger();
+  constructor() {}
 
   catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
 
-    const status =
-      exception instanceof HttpException
-        ? exception.getStatus()
-        : HttpStatus.INTERNAL_SERVER_ERROR;
-
-    const message = this.getExceptionMessage(exception);
     const stack = exception instanceof Error ? exception.stack : undefined;
+    const trace = process.env.NODE_ENV === 'development' ? stack : undefined;
+    const message = this.getExceptionMessage(exception);
     this.logger.error(stack, JSON.stringify(message), 'AllExceptionsFilter');
 
-    response.status(status).json({
+    if (
+      exception instanceof QueryFailedError ||
+      exception instanceof ValidationError
+    ) {
+      return response.status(HttpStatus.BAD_REQUEST).json({
+        success: false,
+        statusCode: HttpStatus.BAD_REQUEST,
+        timestamp: new Date().toISOString(),
+        trace,
+      });
+    } else if (exception instanceof HttpException) {
+      return response.status(exception.getStatus()).json({
+        success: false,
+        statusCode: exception.getStatus(),
+        timestamp: new Date().toISOString(),
+        trace,
+      });
+    }
+
+    response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
       success: false,
-      statusCode: status,
+      statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
       timestamp: new Date().toISOString(),
       message,
       trace: process.env.NODE_ENV === 'development' ? stack : undefined,
